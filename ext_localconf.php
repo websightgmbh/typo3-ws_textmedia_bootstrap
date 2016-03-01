204423e919a7ec8e1bd44d230bf0bd0e43e12474
Cedric Ziel <cedric@cedric-ziel.com>
2016-03-01 12:40:41 +0100

    Add correct preview renderer

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,7 @@ if (!defined('TYPO3_MODE')) {
 
 // Register for hook to show preview of tt_content element of CType="wstextmediabootstrap" in page module
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['wstextmediabootstrap']
-    = \TYPO3\CMS\FluidStyledContent\Hooks\TextmediaPreviewRenderer::class;
+    = Websight\WsTextmediaBootstrap\Preview\WsTextmediaPreviewRenderer::class;
 
 if (TYPO3_MODE === 'BE') {
     call_user_func(
