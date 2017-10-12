00a10b7b0a8bff2a2ee64a061ae757a9b63dc0ef
Florian Obwegs <florian.obwegs@raiffeisen.net>
2017-10-12 15:06:44 +0200

    Revert "Change backend input and frontend output to configure each breakpoint."
    This reverts commit 65ddda42d8782f56a5bd7d633a243771e4e07749.

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -40,124 +40,81 @@ call_user_func(function () {
         }
     }
 
-    // Item selection Array
-    $selItemsArr = [
-        [
-            '',
-            '0'
-        ],
-        [
-            '1/12',
-            '1'
-        ],
-        [
-            '2/12',
-            '2'
-        ],
-        [
-            '3/12 (25%)',
-            '3'
-        ],
-        [
-            '4/12',
-            '4'
-        ],
-        [
-            '5/12',
-            '5'
-        ],
-        [
-            '6/12 (50%)',
-            '6'
-        ],
-        [
-            '7/12',
-            '7'
-        ],
-        [
-            '8/12',
-            '8'
-        ],
-        [
-            '9/12 (75%)',
-            '9'
-        ],
-        [
-            '10/12',
-            '10'
-        ],
-        [
-            '11/12',
-            '11'
-        ],
-        [
-            '12/12 (100%)',
-            '12'
-        ],
-        [
-            'hidden/disabled',
-            '-1'
-        ],
-    ];
-
-
     /*
      * Grid palette
      */
     $GLOBALS['TCA']['tt_content']['palettes']['gridSettings'] = [
         'showitem' => '
             ,--div--;Grid,
-            ws_textmedia_bootstrap_image_size_xs;' . $languageFilePrefix . 'col.ws_textmedia_bootstrap_image_size_xs,
-            ws_textmedia_bootstrap_image_size_sm;' . $languageFilePrefix . 'col.ws_textmedia_bootstrap_image_size_sm,
-            ws_textmedia_bootstrap_image_size_md;' . $languageFilePrefix . 'col.ws_textmedia_bootstrap_image_size_md,
-            ws_textmedia_bootstrap_image_size_lg;' . $languageFilePrefix . 'col.ws_textmedia_bootstrap_image_size_lg,
+            ws_textmedia_bootstrap_image_size;' . $languageFilePrefix . 'col.ws_textmedia_bootstrap_image_size,
         '
     ];
 
-
     $ttContentColumns = [
-        'ws_textmedia_bootstrap_image_size_xs' => [
+        'ws_textmedia_bootstrap_image_size' => [
             'exclude' => 1,
-            'label'   => $languageFilePrefix . 'col.ws_textmedia_bootstrap_image_size_xs',
+            'label'   => $languageFilePrefix . 'col.ws_textmedia_bootstrap_image_size',
             'config'  => [
                 'type'       => 'select',
                 'renderType' => 'selectSingle',
-                'items'      => $selItemsArr,
-                'default'    => 6
-            ]
-        ],
-        'ws_textmedia_bootstrap_image_size_sm' => [
-            'exclude' => 1,
-            'label'   => $languageFilePrefix . 'col.ws_textmedia_bootstrap_image_size_sm',
-            'config'  => [
-                'type'       => 'select',
-                'renderType' => 'selectSingle',
-                'items'      => $selItemsArr,
-                'default'    => 6
-            ]
-        ],
-        'ws_textmedia_bootstrap_image_size_md' => [
-            'exclude' => 1,
-            'label'   => $languageFilePrefix . 'col.ws_textmedia_bootstrap_image_size_md',
-            'config'  => [
-                'type'       => 'select',
-                'renderType' => 'selectSingle',
-                'items'      => $selItemsArr,
-                'default'    => 6
-            ]
-        ],
-        'ws_textmedia_bootstrap_image_size_lg' => [
-            'exclude' => 1,
-            'label'   => $languageFilePrefix . 'col.ws_textmedia_bootstrap_image_size_lg',
-            'config'  => [
-                'type'       => 'select',
-                'renderType' => 'selectSingle',
-                'items'      => $selItemsArr,
+                'items'      => [
+                    [
+                        '',
+                        '0'
+                    ],
+                    [
+                        '1/12',
+                        '1'
+                    ],
+                    [
+                        '2/12',
+                        '2'
+                    ],
+                    [
+                        '3/12 (25%)',
+                        '3'
+                    ],
+                    [
+                        '4/12',
+                        '4'
+                    ],
+                    [
+                        '5/12',
+                        '5'
+                    ],
+                    [
+                        '6/12 (50%)',
+                        '6'
+                    ],
+                    [
+                        '7/12',
+                        '7'
+                    ],
+                    [
+                        '8/12',
+                        '8'
+                    ],
+                    [
+                        '9/12 (75%)',
+                        '9'
+                    ],
+                    [
+                        '10/12',
+                        '10'
+                    ],
+                    [
+                        '11/12',
+                        '11'
+                    ],
+                    [
+                        '12/12 (100%)',
+                        '12'
+                    ],
+                ],
                 'default'    => 6
             ]
         ],
     ];
-
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $ttContentColumns);
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content',
--- ext_tables.sql
+++ ext_tables.sql
@@ -2,8 +2,5 @@
 # Table structure for table 'tt_content'
 #
 CREATE TABLE tt_content (
-  ws_textmedia_bootstrap_image_size_xs int(11) DEFAULT '0' NOT NULL,
-  ws_textmedia_bootstrap_image_size_sm int(11) DEFAULT '0' NOT NULL,
-  ws_textmedia_bootstrap_image_size_md int(11) DEFAULT '0' NOT NULL,
-  ws_textmedia_bootstrap_image_size_lg int(11) DEFAULT '0' NOT NULL,
+  ws_textmedia_bootstrap_image_size int(11) DEFAULT '0' NOT NULL,
 );
