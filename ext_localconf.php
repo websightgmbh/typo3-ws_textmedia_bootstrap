379bad45f1760587db3c94cb6fbd006e7e79ca29
Tilman Schlereth <schlereth@websight.de>
2021-04-25 20:40:32 +0200

    Shorten TYPO3_MODE check

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,5 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3_MODE') or die();
 
 // Register for hook to show preview of tt_content element of CType="wstextmediabootstrap" in page module
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['wstextmediabootstrap']
