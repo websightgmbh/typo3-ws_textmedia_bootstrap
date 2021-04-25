27a11b65408f56bc83298483a4c755654369a5d2
Tilman Schlereth <schlereth@websight.de>
2021-04-25 20:03:41 +0200

    Update extension configuration

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,29 +1,24 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title'            => 'Text & Media Element for Bootstrap 3',
-    'description'      => 'Fluid Styled Content overlay and standalone template for Text & Media',
-    'category'         => 'plugin',
-    'author'           => 'Tilman Schlereth, Cedric Ziel',
-    'author_email'     => 'schlereth@websight.de',
-    'author_company'   => 'websight gmbh',
-    'state'            => 'beta',
-    'internal'         => '',
-    'uploadfolder'     => '0',
-    'createDirs'       => '',
-    'modify_tables'    => '',
-    'clearCacheOnLoad' => 0,
-    'version'          => '3.0.0',
-    'constraints'      => [
-        'depends'   => [
-            'typo3' => '8.7.0-8.7.99',
+    'title' => 'Text & Media Element for Bootstrap 3',
+    'description' => 'Fluid Styled Content overlay and standalone template for Text & Media',
+    'version' => '4.0.0',
+    'category' => 'plugin',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '9.5.0-9.5.99',
             'fluid_styled_content' => '',
             'fluid_styled_responsive_images' => '',
         ],
-        'conflicts' => [],
-        'suggests'  => [],
     ],
-    'autoload'         => [
-        'psr-4' => ['Websight\\WsTextmediaBootstrap\\' => 'Classes']
-    ]
+    'state' => 'beta',
+    'author' => 'Tilman Schlereth, Cedric Ziel',
+    'author_email' => 'schlereth@websight.de',
+    'author_company' => 'websight gmbh',
+    'autoload' => [
+        'psr-4' => [
+            'Websight\\WsTextmediaBootstrap\\' => 'Classes'
+        ]
+    ],
 ];
