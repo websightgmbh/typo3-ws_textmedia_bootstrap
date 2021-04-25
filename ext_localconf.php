74fbef90beaf33b035ba39747e0e247fd764b0be
Tilman Schlereth <schlereth@websight.de>
2021-04-25 20:47:15 +0200

    Use a directly called closure function in localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,21 +1,23 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-// Register for hook to show preview of tt_content element of CType="wstextmediabootstrap" in page module
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['wstextmediabootstrap']
-    = Websight\WsTextmediaBootstrap\Preview\WsTextmediaPreviewRenderer::class;
+(function () {
+    // Register for hook to show preview of tt_content element of CType="wstextmediabootstrap" in page module
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['wstextmediabootstrap']
+        = Websight\WsTextmediaBootstrap\Preview\WsTextmediaPreviewRenderer::class;
 
-if (TYPO3_MODE === 'BE') {
-    call_user_func(
-        function ($extKey) {
-            // Get the extension configuration
-            $extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$extKey]);
+    if (TYPO3_MODE === 'BE') {
+        call_user_func(
+            function ($extKey) {
+                // Get the extension configuration
+                $extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$extKey]);
 
-            if (!isset($extConf['loadContentElementWizardTsConfig']) || (int)$extConf['loadContentElementWizardTsConfig'] === 1) {
-                // Include new content elements to modWizards
-                \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ws_textmedia_bootstrap/Configuration/PageTSconfig/NewContentElementWizard.typoscript">');
-            }
-        },
-        $_EXTKEY
-    );
-}
+                if (!isset($extConf['loadContentElementWizardTsConfig']) || (int)$extConf['loadContentElementWizardTsConfig'] === 1) {
+                    // Include new content elements to modWizards
+                    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ws_textmedia_bootstrap/Configuration/PageTSconfig/NewContentElementWizard.typoscript">');
+                }
+            },
+            $_EXTKEY
+        );
+    }
+})();
