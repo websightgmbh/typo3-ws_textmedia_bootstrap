3e7ec06d708d2aa5c024cf660fc068f25426283d
Tilman Schlereth <schlereth@websight.de>
2021-04-26 17:55:06 +0200

    Cleanup new CE wizard config and language files

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,12 +2,12 @@
 defined('TYPO3_MODE') or die();
 
 (function () {
-    // Register for hook to show preview of tt_content element of CType="wstextmediabootstrap" in page module
+    // Register hook to show preview of tt_content element of standalone CType="wstextmediabootstrap" in page module
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['wstextmediabootstrap']
         = Websight\WsTextmediaBootstrap\Preview\WsTextmediaPreviewRenderer::class;
 
     if (TYPO3_MODE === 'BE') {
-        // Include new content elements to modWizards
+        // Add standalone version to new CE wizard
         if ((bool)TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('ws_textmedia_bootstrap', 'loadContentElementWizardTsConfig') === true) {
             TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ws_textmedia_bootstrap/Configuration/PageTSconfig/NewContentElementWizard.typoscript">');
         }
